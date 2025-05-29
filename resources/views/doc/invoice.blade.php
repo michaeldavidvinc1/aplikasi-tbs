768d4df026066907d4292c3249f3df30e9df9957
michaeldavidvinc1 <tawie18102000@gmail.com>
2025-05-29 20:50:11 +0700

    Update design invoice

--- resources/views/doc/invoice.blade.php
+++ resources/views/doc/invoice.blade.php
@@ -1,17 +1,130 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
-    <title>Invoice</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta charset="UTF-8">
+    <title>Bukti Pembayaran Hasil TBS</title>
     <style>
-        .page-break {
-            page-break-after: always;
+        body {
+            font-family: sans-serif;
+            font-size: 12px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 12px;
+        }
+        th, td {
+            border: 1px solid #000;
+            padding: 4px;
+            text-align: center;
+        }
+        .no-border td, .no-border th {
+            border: none;
+            text-align: left;
+            padding: 2px;
+        }
+        .text-right {
+            text-align: right;
+        }
+        .text-left {
+            text-align: left;
         }
     </style>
 </head>
 <body>
-<h1>Invoice #{{ $transaksi->offer->user->name }}</h1>
-<p>Nama: {{ $transaksi->offer->berat }}</p>
-<p>Total: Rp{{ number_format($transaksi->total_bayar, 0, ',', '.') }}</p>
+
+<h3 style="text-align: center; margin-bottom: 5px;">KOPERASI UNIT DESA "TUNAS MUDA"</h3>
+<p style="text-align: center; margin: 0;">KAMPUNG TELUK MERBAU KEC DAYUN KAB SIAK</p>
+<h4 style="text-align: center; margin-top: 5px;">BUKTI PEMBAYARAN HASIL TBS</h4>
+
+<table class="no-border">
+    <tr>
+        <td>Nama</td>
+        <td>: Supriyono</td>
+        <td>Kelompok</td>
+        <td>: 2</td>
+    </tr>
+    <tr>
+        <td>Bulan</td>
+        <td>: Januari 2025</td>
+    </tr>
+</table>
+
+<table>
+    <thead>
+    <tr>
+        <th>NO</th>
+        <th>Harga Kotor</th>
+        <th>Insentif</th>
+        <th>Timbang di</th>
+        <th>Angkut</th>
+        <th>Harga Bersih</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr><td>I</td><td>2,837</td><td>85</td><td>100</td><td>2,652.44</td></tr>
+    <tr><td>II</td><td>2,664</td><td>85</td><td>100</td><td>2,479.00</td></tr>
+    <tr><td>III</td><td>2,665</td><td>85</td><td>100</td><td>2,480.00</td></tr>
+    <tr><td>IV</td><td colspan="4" class="text-right">Total Pengurangan</td><td>(185.00)</td></tr>
+    </tbody>
+</table>
+
+<table>
+    <thead>
+    <tr>
+        <th>PANEN</th>
+        <th>TONASE</th>
+        <th>JUMLAH HARGA</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr><td>I</td><td>3,455</td><td>9,164,180</td></tr>
+    <tr><td>II</td><td>3,200</td><td>7,932,800</td></tr>
+    <tr><td>III</td><td>3,150</td><td>7,812,000</td></tr>
+    <tr><td>IV</td><td></td><td></td></tr>
+    <tr><th>JUMLAH</th><th>9,805</th><th>24,908,980</th></tr>
+    </tbody>
+</table>
+
+<h4>TABUNGAN DAN POTONGAN:</h4>
+
+<table>
+    <thead>
+    <tr>
+        <th>TABUNGAN</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr><td>1. Peremajaan</td><td>-</td></tr>
+    <tr><td>2. Korban</td><td>-</td></tr>
+    <tr><td>3. Pembiayaan</td><td>-</td></tr>
+    </tbody>
+</table>
+
+<table>
+    <thead>
+    <tr>
+        <th>POTONGAN</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr><td>1. Bank</td><td>7,700,792</td></tr>
+    <tr><td>2. Pupuk</td><td>3,846,539</td></tr>
+    <tr><td>3. Korban</td><td>250,000</td></tr>
+    </tbody>
+    <tfoot>
+    <tr>
+        <th>JUMLAH POTONGAN</th>
+        <th>11,797,331</th>
+    </tr>
+    <tr>
+        <th>TERIMA BERSIH</th>
+        <th>13,111,649</th>
+    </tr>
+    </tfoot>
+</table>
+
 </body>
 </html>
