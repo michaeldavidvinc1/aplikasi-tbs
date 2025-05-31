c169c51795e449ee8f4290b2fdce42e1324ade24
michaeldavidvinc1 <tawie18102000@gmail.com>
2025-05-31 11:13:22 +0700

    Update riwayat pembayaran

--- resources/views/livewire/petani/riwayat-pembayaran.blade.php
+++ resources/views/livewire/petani/riwayat-pembayaran.blade.php
@@ -31,8 +31,8 @@
                 <thead class="bg-gray-100">
                 <tr>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">#</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Berat</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Kualitas</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Tonase</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Kualitas TBS</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Harga Beli</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Total Harga</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Status</th>
@@ -43,7 +43,7 @@
                 @forelse ($pembayarans as $index => $item)
                     <tr>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $pembayarans->firstItem() + $index }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $item->offer->berat }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $item->offer->tonase }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $item->offer->kualitas }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $item->harga_beli }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $item->total_bayar }}</td>
