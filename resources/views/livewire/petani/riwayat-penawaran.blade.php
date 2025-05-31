819650d0bb2cf8f00d362c826d18956d91229b99
michaeldavidvinc1 <tawie18102000@gmail.com>
2025-05-31 11:10:11 +0700

    Update riwayat penwaran

--- resources/views/livewire/petani/riwayat-penawaran.blade.php
+++ resources/views/livewire/petani/riwayat-penawaran.blade.php
@@ -32,8 +32,8 @@
                 <thead class="bg-gray-100">
                 <tr>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">#</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Berat</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Kualitas</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Tonase</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Kualitas TBS</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Lokasi</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Status</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">Tanggal</th>
@@ -43,7 +43,7 @@
                 @forelse ($penawarans as $index => $item)
                     <tr>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $penawarans->firstItem() + $index }}</td>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $item->berat }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $item->tonase }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $item->kualitas }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $item->lokasi }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">
