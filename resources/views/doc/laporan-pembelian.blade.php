2ee5e22eff00e325d92dd4d719d2a537c5ca0367
michaeldavidvinc1 <tawie18102000@gmail.com>
2025-07-11 22:53:27 +0700

    Perbaikan cetak laporan design

--- resources/views/doc/laporan-pembelian.blade.php
+++ resources/views/doc/laporan-pembelian.blade.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Bukti Pembayaran Hasil TBS</title>
+    <title>Laporan Pembelian TBS</title>
     <style>
         body {
             font-family: sans-serif;
@@ -12,79 +12,85 @@
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 12px;
         }
         th, td {
             border: 1px solid #000;
-            padding: 4px;
+            padding: 6px;
             text-align: center;
         }
-        .no-border td, .no-border th {
+        .no-border td {
             border: none;
-            text-align: left;
             padding: 2px;
         }
-        .text-right {
-            text-align: right;
-        }
-        .text-left {
-            text-align: left;
-        }
-        .text-center {
-            text-align: center;
-        }
-        .header {
-            margin-bottom: 15px;
-        }
-        .kop {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            margin-bottom: 10px;
-        }
-        .kop img {
-            width: 80px;
-            height: auto;
-        }
         .kop-title {
-            flex-grow: 1;
             text-align: center;
             font-size: 16px;
             font-weight: bold;
             text-transform: uppercase;
         }
+        .subkop {
+            text-align: center;
+            font-size: 12px;
+            margin-top: 4px;
+        }
+        .email {
+            text-align: center;
+            font-size: 11px;
+            margin-top: 2px;
+        }
+        .logo {
+            width: 80px;
+        }
+        .status-badge {
+            padding: 2px 6px;
+            font-size: 10px;
+            font-weight: bold;
+            border-radius: 4px;
+            display: inline-block;
+        }
+        .sudah-bayar {
+            background-color: #c8f7c5;
+            color: #2b662e;
+        }
         .footer {
             margin-top: 30px;
+            display: flex;
+            justify-content: space-between;
         }
         .ttd {
             display: flex;
             justify-content: space-between;
-            align-items: center;
+            margin-top: 40px;
+        }
+        .ttd div {
+            width: 30%;
+            text-align: center;
+        }
+        .ttd .center {
+            width: 30%;
         }
     </style>
 </head>
 <body>
 
-<!-- Kop Surat -->
-<div class="kop" style="width: 100%; margin-bottom: 10px;">
-    <table style="width: 100%; border: none; border-collapse: collapse;">
-        <tr>
-            <td style="width: 80px; border: none;">
-                <img src="./pohon.jpg" alt="Logo Kiri" style="width: 80px;">
-            </td>
-            <td style="text-align: center; border: none;">
-                <div style="font-size: 16px; font-weight: bold; text-transform: uppercase;">Laporan Pembelian</div>
-                <div style="font-size: 14px; margin-top: 4px;">Koperasi Produsen Tunas Muda</div>
-            </td>
-            <td style="width: 80px; text-align: right; border: none;">
-                <img src="./bunga.jpg" alt="Logo Kanan" style="width: 80px;">
-            </td>
-        </tr>
-    </table>
-</div>
+<!-- Header -->
+<table class="no-border">
+    <tr>
+        <td style="width: 80px;"><img src="{{ public_path('./pohon.jpg') }}" class="logo" alt="logo kiri"></td>
+        <td style="text-align: center;">
+            <div class="kop-title">KOPERASI PRODUSEN <br><span style="color: red;">“TUNAS MUDA“</span></div>
+            <div class="subkop">Jl. Panglima Besar Kampung Teluk Merbau Kec.dayun Kab.Siak – Riau</div>
+            <div class="email">Email. Kud.tunasmuda1991@gmail.com</div>
+            <div class="email">Kode Pos. 28656</div>
+        </td>
+        <td style="width: 80px;"><img src="{{ public_path('./bunga.jpg') }}" class="logo" alt="logo kanan"></td>
+    </tr>
+</table>
 
-<!-- Garis Bawah Kop -->
-<div style="border-top: 2px solid #000; margin-bottom: 20px;"></div>
+<hr style="margin: 10px 0; border: 1px solid black;">
+
+<!-- Judul -->
+<h3 style="text-align: center; margin-bottom: 10px;">Laporan Pembelian TBS</h3>
 
 <!-- Tabel Transaksi -->
 <table>
@@ -101,7 +107,9 @@
     </tr>
     </thead>
     <tbody>
+    @php $totalHarga = 0; @endphp
     @forelse ($transaksi as $index => $item)
+        @php $totalHarga += $item->total_bayar; @endphp
         <tr>
             <td>{{ $index + 1 }}</td>
             <td>{{ $item->offer->user->name }}</td>
@@ -110,16 +118,9 @@
             <td>{{ number_format($item->harga_beli, 0, ',', '.') }}</td>
             <td>{{ number_format($item->total_bayar, 0, ',', '.') }}</td>
             <td>
-                @php
-                    $color = match($item->status) {
-                        'belum bayar' => 'background-color:#eee; color:#555;',
-                        'sudah bayar' => 'background-color:#c8f7c5; color:#2b662e;',
-                        default => 'background-color:#eee; color:#555;',
-                    };
-                @endphp
-                <span style="padding: 2px 6px; font-size: 10px; font-weight: bold; border-radius: 4px; {{ $color }}">
-                    {{ ucfirst($item->status) }}
-                </span>
+                    <span class="status-badge sudah-bayar">
+                        {{ ucfirst($item->status) }}
+                    </span>
             </td>
             <td>{{ $item->created_at->format('d M Y') }}</td>
         </tr>
@@ -130,6 +131,41 @@
     @endforelse
     </tbody>
 </table>
+
+<!-- Total -->
+<table style="width: 50%; float: right; margin-top: 10px;">
+    <tr>
+        <td><strong>Total Harga yang Diterima</strong></td>
+    </tr>
+    <tr>
+        <td>Jumlah: <strong>Rp {{ number_format($totalHarga, 0, ',', '.') }}</strong></td>
+    </tr>
+</table>
+
+<div style="clear: both;"></div>
+
+<!-- TTD -->
+<!-- Footer Tanda Tangan -->
+<div style="width: 100%; margin-top: 60px;">
+
+    <!-- Kiri: Diketahui Oleh -->
+    <div style="width: 45%; float: left; text-align: center;">
+        <div style="margin-bottom: 60px;">Diketahui Oleh</div>
+        <div>( Pimpinan )</div>
+    </div>
+
+    <!-- Kanan: Dayun, Tanggal & Koperasi -->
+    <div style="width: 45%; float: right; text-align: center;">
+        <div>Dayun, .................. 20</div>
+        <div style="margin-bottom: 60px;">Dibuat Oleh</div>
+        <div>( Koperasi )</div>
+    </div>
+
+</div>
+
+<!-- Clear Float -->
+<div style="clear: both;"></div>
+
 
 </body>
 </html>
